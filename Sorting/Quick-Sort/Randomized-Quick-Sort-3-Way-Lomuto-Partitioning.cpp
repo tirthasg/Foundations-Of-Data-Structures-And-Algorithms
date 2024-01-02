1830acd6c35d2fb46526700b9737a7b82139f66e
Tirtha <tirthasg@gmail.com>
2024-01-03 00:02:25 +0530

    feat: Randomized Quick sort using 3-way Lomuto Partitioning added to repo

---
+++ Sorting/Quick-Sort/Randomized-Quick-Sort-3-Way-Lomuto-Partitioning.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include <algorithm>
+using namespace std;
+
+#include "../../Utility-Functions/Input-Vector.h"
+#include "../../Utility-Functions/Print-Vector.h"
+
+void Helper(vector<int> &nums, int start, int end)
+{
+    if (start >= end)
+        return;
+
+    srand(time(NULL));
+    int randomIndex = rand() % (end - start + 1) + start;
+
+    swap(nums[start], nums[randomIndex]);
+
+    int pivot = nums[start];
+    int smaller = start;
+    int equal = start;
+    
+    for (int bigger = start + 1; bigger <= end; ++bigger)
+        if (nums[bigger] == pivot)
+            swap(nums[++equal], nums[bigger]);
+        else if (nums[bigger] < pivot) {
+            swap(nums[++equal], nums[bigger]);
+            swap(nums[++smaller], nums[equal]);
+        }
+
+    swap(nums[start], nums[smaller]);
+
+    Helper(nums, start, smaller - 1);
+    Helper(nums, equal + 1, end);
+}
+
+void QuickSort(vector<int> &nums)
+{
+    int N = nums.size();
+
+    Helper(nums, 0, N - 1);
+}
+
+int main()
+{
+    vector<int> nums = InputVector();
+
+    QuickSort(nums);
+    PrintVector(nums);
+
+    return 0;
+}
