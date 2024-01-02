777105e81fbae1f321d6b59073a0b167206a18da
Tirtha <tirthasg@gmail.com>
2024-01-02 23:23:10 +0530

    feat: Quick sort using 2-way Lomuto partitioning added to repo

---
+++ Sorting/Quick-Sort/Quick-Sort-Lomuto-Partitioning.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+#include "../../Utility-Functions/Input-Vector.h"
+#include "../../Utility-Functions/Print-Vector.h"
+
+void Helper(vector<int> &nums, int start, int end)
+{
+    if (start >= end)
+        return;
+
+    int pivot = nums[start];
+    int smaller = start;
+    for (int bigger = start + 1; bigger <= end; ++bigger)
+        if (nums[bigger] < pivot)
+            swap(nums[++smaller], nums[bigger]);
+
+    swap(nums[start], nums[smaller]);
+
+    Helper(nums, start, smaller - 1);
+    Helper(nums, smaller + 1, end);
+}
+
+void QuickSort(vector<int> &nums)
+{
+    int N = nums.size();
+
+    Helper(nums, 0, N - 1);
+}
+
+int main()
+{
+    vector<int> nums = InputVector();
+
+    QuickSort(nums);
+    PrintVector(nums);
+
+    return 0;
+}
