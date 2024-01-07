28ac44ec50d1ea8a7c68286c5f13a58671c6fe52
Tirtha <tirthasg@gmail.com>
2024-01-07 16:11:44 +0530

    feat: added boilerplate code for Heap sort

---
+++ Sorting/Heap-Sort/Heap-Sort.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#include "../../Utility-Functions/Input-Vector.h"
+#include "../../Utility-Functions/Print-Vector.h"
+
+void HeapSort(vector<int> nums)
+{;}
+
+int main()
+{
+    vector<int> nums = InputVector();
+
+    HeapSort(nums);
+    PrintVector(nums);
+
+    return 0;
+}
